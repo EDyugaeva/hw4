1ef83a5f3ae7ce3826b545cfb2c0fe83f7434a95
Ekaterina <ed2408@yandex.ru>
2021-11-23 20:02:22 +0300

    First commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,41 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        task1();
+        task2();
+//        task3();
+    }
+
+    public static void task1() {
+        byte number = 0;
+        while (number < 10) {
+            number++;
+            System.out.println(number + " ");
+        }
+        for (byte i = 10; i > 0; i = (byte) (i - 1)) {
+            System.out.println(i + " ");
+        }
+    }
+
+    public static void task2() {
+        int firstFriday = 3;
+        int n = 0;
+        for (int day = 0; day < 32; day = day + 1) {
+            if (day == firstFriday + 7 * n) {
+                System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+                n = n + 1;
+            }
+        }
+    }
+
+    public static void task3() {
+        int year = 2021;
+        int last = year - 200;
+        int future = year + 100;
+        for (int a = last; a < future; a = a + 1) {
+            if (a == 1896 || a == 1975 || a == 2054) {
+                System.out.println(a);
+            }
+        }
     }
 }
