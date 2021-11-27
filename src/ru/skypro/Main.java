74a6713a3408dd10ca43e0285fdaa28654f1c5e6
Ekaterina <ed2408@yandex.ru>
2021-11-27 12:32:18 +0300

    Second commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,27 +3,30 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        task1();
-        task2();
+//        task1();
+//        task2();
         task3();
     }
 
     public static void task1() {
-        int number = 0;
-        while (number < 10) {
-            number++;
+        int number = 1;
+        while (number <= 10) {
+            if (number == 10) {
+                System.out.println(number);
+                break;
+            }
             System.out.print(number + " ");
+            number++;
         }
-        System.out.println();
-        for (int i = 10; i > 0; i = (i - 1)) {
-            System.out.print(i + " ");
+        for (; number > 0; number--) {
+            System.out.print(number + " ");
         }
         System.out.println();
     }
 
     public static void task2() {
         int friday = 3;
-        for (; friday < 32; friday = friday + 7) {
+        for (; friday <= 31; friday += 7) {
             System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
         }
     }
@@ -32,9 +35,9 @@ public class Main {
         int currentYear = 2021;
         int last = currentYear - 200;
         int future = currentYear + 100;
-        for (int comet = 0; comet < future; comet = comet + 79) {
-            if (comet > last) {
-                System.out.println(comet);
+        for (int i = last; i < future; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
             }
         }
     }
